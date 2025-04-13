99159c3d6669d83bc03c76bc26546b35a2bad4e8
David Hunt <david.hunt@duke.edu>
2025-04-13 16:16:18 -0400

    added bringup package, added ability to have a custom prefix when publishing tf tree

---
+++ src/create3_bringup/launch/create3_bringup.launch.py
@@ -0,0 +1,56 @@
+from ament_index_python.packages import get_package_share_directory
+import os
+from launch import LaunchDescription
+from launch.actions import (
+    DeclareLaunchArgument,
+    GroupAction,
+    IncludeLaunchDescription,
+    OpaqueFunction
+)
+from launch.conditions import IfCondition, UnlessCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import PushRosNamespace, SetRemap, Node
+from launch_ros.descriptions import ComposableNode, ParameterFile
+from nav2_common.launch import RewrittenYaml
+
+
+#locating other packages
+pkg_tf_repub = get_package_share_directory('tf_repub')
+
+#ROS2 launch arguments
+ARGUMENTS = [
+    DeclareLaunchArgument('namespace', default_value='',
+                          description='namespace')                         
+]
+
+def launch_setup(context, *args, **kwargs):
+
+    #load parameters
+    namespace = LaunchConfiguration('namespace')
+
+    #updating paths
+    namespace_str = namespace.perform(context)
+    if (namespace_str and not namespace_str.startswith('/')):
+        namespace_str = '/' + namespace_str
+
+    # Apply the following re-mappings only within this group
+    bringup_group = GroupAction([
+        PushRosNamespace(namespace),
+
+        Node(
+            package='tf_repub',
+            executable='tf_repub',
+            name='tf_repub',
+            output='screen',
+            parameters=[]
+        )
+    ])
+
+    return [bringup_group]
+
+
+def generate_launch_description():
+    ld = LaunchDescription(ARGUMENTS)
+    ld.add_action(OpaqueFunction(function=launch_setup))
+    return ld
--- src/tf_repub/tf_repub/tf_repub.py
+++ src/tf_repub/tf_repub/tf_repub.py
@@ -1,15 +1,45 @@
 import rclpy
 from rclpy.node import Node
 from tf2_msgs.msg import TFMessage
+from geometry_msgs.msg import TransformStamped
 
 class TFRelay(Node):
     def __init__(self):
         super().__init__('tf_relay')
-        self.subscription = self.create_subscription(TFMessage, '/tf', self.tf_callback, 10)
-        self.publisher = self.create_publisher(TFMessage, '/forwarded_tf', 10)
 
-    def tf_callback(self, msg):
-        self.publisher.publish(msg)
+        #specify the namespace
+        self.namespace = self.get_namespace()
+        if self.namespace == '/':
+            self.namespace = ''
+            self.tf_prefix = ''
+        else:
+            self.tf_prefix = "{}/".format(self.namespace)
+
+        #setup the subscription to the tf frame
+        self.subscription = self.create_subscription(
+            TFMessage,
+            '/cpsl_ugv_1/tf',
+            self.tf_callback,
+            1
+        )
+        self.publisher = self.create_publisher(
+            TFMessage,
+            '/tf',
+            1
+        )
+
+    def tf_callback(self, msg: TFMessage):
+        filtered_transforms = []
+        t:TransformStamped = None
+        for t in msg.transforms:
+            if t.header.frame_id == 'odom' and t.child_frame_id == 'base_link':
+                t.header.frame_id = "{}odom".format(self.tf_prefix)
+                t.child_frame_id = "{}base_link".format(self.tf_prefix)
+                filtered_transforms.append(t)
+
+        if filtered_transforms:
+            filtered_msg = TFMessage(transforms=filtered_transforms)
+            self.publisher.publish(filtered_msg)
 
 def main(args=None):
     rclpy.init(args=args)
